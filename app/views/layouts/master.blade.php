6732ab3b026d7bfddd4bb4c50cc34d2e22bc305f
wazaap <thomas@leonhard.dk>
2014-09-09 17:48:12 +0200

    Jquery and Bootstrap
    Jquery link added
    Bootstrap link added
    Bootstrap style link added

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,6 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Olsen Ejendomme</title>
+	<link rel="stylesheet" type="text/css" href="../app/css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
 </head>
 <body>
 @yield('content')
