1fa80990a95e749dbc25f3e60e57ecc20f77d323
Mads Dam Eckardt Jensen <mdej35@hotmail.com>
2014-09-10 15:40:09 +0200

    Styling of loginpage

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -11,27 +11,37 @@
 <body>
 
 	<div id="loginbox" class="container-fluid loginbox">
-	<div class="top">Log ind</div>
-	<br>
+		<div class="top">Log ind</div>
+		<br>
 		{{Form::open(['route'=>'sessions.store', 'class' => 'form-inline']) }}
-		{{Form::text('email','', ['class' => 'form-control', 'style' => 'width:100%']) }}
+		{{Form::text('email','user@example.com', ['class' => 'form-control', 'style' => 'width:100%']) }}
 		<br>
 		{{Form::password('password', ['class' => 'form-control', 'style' => 'width:100%'])}}
 		<br>
 		{{ HTML::link('http://test.com', 'Glemt password?')}}
 		<br>
-		{{Form::select('selskab',
-			array(
-				'e1 1' => 'Selskab 1',
-				'e2 2' => 'Selskab 2',
-				'e3 3' => 'Selskab 3',
-				'e3 4' => 'Selskab 4',
-				'e4 5' => 'Selskab 5'
-				),'',
-			['class' => 'form-control']
-			)
-		}}{{Form::submit('Login', ['class' => 'btn btn-success'])}}
-		{{ Form::Close()}}
+		<table style="width:100%;">
+			<tr>
+				<td style="width:100%;">
+					{{Form::select('selskab',
+						array(
+							'e1 1' => 'Selskab 1',
+							'e2 2' => 'Selskab 2',
+							'e3 3' => 'Selskab 3',
+							'e3 4' => 'Selskab 4',
+							'e4 5' => 'Selskab 5'
+							),'',
+						['class' => 'form-control', 'style' => 'width:100%;']
+						)
+					}}
+				</td>
+				<td>
+
+					{{Form::submit('Login', ['class' => 'btn btn-success loginbutton'])}}
+					{{ Form::Close()}}
+				</td>
+			</tr>
+		</table>
 	</div>
 </body>
 </html>
