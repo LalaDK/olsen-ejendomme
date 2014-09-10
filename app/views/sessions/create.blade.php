3c0c8f33204e3294ed53f3a1ed180b77e4e8702d
Mads Dam Eckardt Jensen <mdej35@hotmail.com>
2014-09-10 12:07:35 +0200

    Migrate

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -1,6 +1,14 @@
-@extends('layouts.master')
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Olsen Ejendomme A/S</title>
+	<link rel="stylesheet" type="text/css" href="../app/css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-@section('content')
 <div id="loginbox" class="container-fluid">
 	<p>Velkommen! Log ind for at adgang til systemet</p>
 	<div>
@@ -13,7 +21,19 @@
 		{{Form::password('password')}}
 	</div>
 
+	<div>
+		{{
+			Form::select(
+				array(
+					'Ejendomsselskab 1' => 
+					)
+			)
+			}}
+	</div>
+
 	{{Form::submit('Login')}}
 	{{ Form::Close()}}
 </div>
-@stop
+
+</body>
+</html>
