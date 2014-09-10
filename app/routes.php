b7c7c20bfeb51ebdfc58b85e299dd1e33b19818a
wazaap <thomas@leonhard.dk>
2014-09-10 10:52:11 +0200

    Login fixed
    Temp fix for login.

--- app/routes.php
+++ app/routes.php
@@ -1,15 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+Route::get('/', function()
+{
+	return View::make('sessions.create');
+});
+
 Route::resource('users','UserController');
 
 Route::get('login','SessionsController@create');
@@ -17,3 +12,4 @@ Route::get('login','SessionsController@create');
 Route::get('logout','SessionsController@destroy');
 
 Route::resource('sessions','SessionsController');
+
