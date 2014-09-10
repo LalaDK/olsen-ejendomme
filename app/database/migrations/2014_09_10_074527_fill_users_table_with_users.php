4a0fae1b2efbe46e38723f0739fbd1087def09ff
Mads Dam Eckardt Jensen <mdej35@hotmail.com>
2014-09-10 10:00:35 +0200

    Migration script
    Created migration script for inserting two users; 'admin' and 'user'
    with default password: 'qwerty'.

---
+++ app/database/migrations/2014_09_10_074527_fill_users_table_with_users.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FillUsersTableWithUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$date = new \DateTime;
+
+		DB::table('users')->insert(
+			array('email' => 'admin',
+				'password' => 'qwerty',
+				'created_at' => $date,
+				'updated_at' => $date
+				)
+			);
+
+
+		DB::table('users')->insert(
+			array('email' => 'user',
+				'password' => 'qwerty',
+				'created_at' => $date,
+				'updated_at' => $date
+				)
+			);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::table('users')->where('email', '=', 'admin')->delete();
+	}
+}
