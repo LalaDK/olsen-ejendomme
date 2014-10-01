7a3438f4d5ea56951f6271745fb588106d2b7c29
wazaap <thomas@leonhard.dk>
2014-10-01 14:59:12 +0200

    Updated dashboard index

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 
 <script>
-	$(document).ready(function(){
-		setselected("#dashboard");
-		$('#companyTabs a:first').tab('show');
-	});
+$(document).ready(function(){
+	setselected("#dashboard");
+	$('#companyTabs a:first').tab('show');
+});
 </script>
 
 <h3>Dashboard</h3>
@@ -62,7 +62,7 @@
 				</div>
 			</div>
 		</div>
-<<<<<<< HEAD
+		<<<<<<< HEAD
 	</div>
 </div>
 <div class="col-md-10">
@@ -98,44 +98,11 @@
 				@else
 				Der er ingen bygninger i dette firma...
 				@endif
-=======
-		<div class="col-md-10">
-			<ul class="nav nav-tabs" data-tabs="tabs" id="companyTabs">
-				@foreach ($companies as $company) 
-				<li><a href="#{{$company->id}}" role="tab" data-toggle="tab">{{$company->name}}</a></li>
-				@endforeach
-			</ul>
-			<div class="tab-content">
-				@foreach ($companies as $company) 
-				<div class="tab-pane" id="{{$company->id}}">
-					<div class="col-md-12">
-						@if ($company->realestates->count() > 0)
-						<br>
-						<table class="table-stribed table-curved" style="width:100%">
-							<th>Id</th>
-							<th>Adresse</th>
-							<th>Matrikel nr.</th>
-							<th>Antal lejemål</th>
-							<th></th>
-							@foreach ($company->realestates as $estate)
-							<tr>
-								<td>{{$estate->id}}</td>
-								<td>{{$estate->address}}</td>
-								<td>{{$estate->cadastral_number}}</td>
-								<td>{{$estate->leases}}</td>
-								<td><span class="glyphicon glyphicon-chevron-right"></span></td>
-							</tr>
-							@endforeach
-						</table>
-						@else
-						Der er ingen bygninger i dette firma...
-						@endif
->>>>>>> origin/master
 
-					</div>
-				</div>
-				@endforeach
 			</div>
 		</div>
+		@endforeach
+	</div>
+</div>
 
-		@stop
+@stop
