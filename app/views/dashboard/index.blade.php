540e2266b82ad2b8775c6120071745c3980d9ec4
wazaap <thomas@leonhard.dk>
2014-10-01 15:58:45 +0200

    Dashboard and css updated

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -34,13 +34,16 @@ $(document).ready(function(){
 				</div>
 				<div class="panel-body">
 					<div class="btn-new-item-background">
-						<button type="button" class="btn btn-default btn-new-item">Opret nyt selskab</button>
-						<span class="glyphicon glyphicon-plus glyphicon-center"></span>
-
+						<a href="companies/create" target="_self" class="litebox">
+							<div class="btn-new-item">Opret nyt selskab</div>
+							<div class="glyphicon glyphicon-plus glyphicon-center"></div>
+						</a>
 					</div>
 					<div class="btn-delete-item-background">
-						<button type="button" class="btn btn-default btn-delete-item">Slet selskab</button>
-						<span class="glyphicon glyphicon-minus glyphicon-center"></span>
+						<a href="companies/delete" target="_self" class="litebox">
+							<div class="btn-delete-item">Slet selskab</div>
+							<div class="glyphicon glyphicon-minus glyphicon-center"></div>
+						</a>
 					</div>
 				</div>
 			</div>
@@ -52,56 +55,60 @@ $(document).ready(function(){
 				</div>
 				<div class="panel-body">
 					<div class="btn-new-item-background">
-						<button type="button" class="btn btn-default btn-new-item">Opret ny ejendom</button>
-						<span class="glyphicon glyphicon-plus glyphicon-center"></span>
+						<a href="realestates/create" target="_self" class="litebox">
+							<div class="btn-new-item">Opret ny ejendom</div>
+							<div class="glyphicon glyphicon-plus glyphicon-center"></div>
+						</a>
 					</div>
 					<div class="btn-delete-item-background">
-						<button type="button" class="btn btn-default btn-delete-item">Slet ejendom</button>
-						<span class="glyphicon glyphicon-minus glyphicon-center"></span>
+						<a href="realestates/delete" target="_self" class="litebox">
+							<div class="btn-delete-item">Slet ejendom</div>
+							<div class="glyphicon glyphicon-minus glyphicon-center"></div>
+						</a>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-<div class="col-md-10">
-	<ul class="nav nav-tabs" data-tabs="tabs" id="companyTabs">
-		@foreach ($companies as $company) 
-		<li><a href="#{{$company->id}}" role="tab" data-toggle="tab">{{$company->name}}</a></li>
-		@endforeach
-	</ul>
-	<div class="tab-content">
-		@foreach ($companies as $company) 
-		<div class="tab-pane" id="{{$company->id}}">
-			<div class="col-md-12">
-				@if ($company->realestates->count() > 0)
-				<br>
-				<table class="table-stribed table-curved" style="width:100%">
-					<th>Id</th>
-					<th>Adresse</th>
-					<th>Nr</th>
-					<th>Matrikel nr.</th>
-					<th>Antal lejemål</th>
-					<th></th>
-					@foreach ($company->realestates as $estate)
-					<tr>
-						<td>{{$estate->id}}</td>
-						<td>{{$estate->street_name}}</td>
-						<td>{{$estate->street_number}}</td>
-						<td>{{$estate->cadastral_number}}</td>
-						<td>{{$estate->leases}}</td>
-						<td><span class="glyphicon glyphicon-chevron-right"></span></td>
-					</tr>
-					@endforeach
-				</table>
-				@else
-				Der er ingen bygninger i dette firma...
-				@endif
+	<div class="col-md-10">
+		<ul class="nav nav-tabs" data-tabs="tabs" id="companyTabs">
+			@foreach ($companies as $company) 
+			<li><a href="#{{$company->id}}" role="tab" data-toggle="tab">{{$company->name}}</a></li>
+			@endforeach
+		</ul>
+		<div class="tab-content">
+			@foreach ($companies as $company) 
+			<div class="tab-pane" id="{{$company->id}}">
+				<div class="col-md-12">
+					@if ($company->realestates->count() > 0)
+					<br>
+					<table class="table-stribed table-curved" style="width:100%">
+						<th>Id</th>
+						<th>Adresse</th>
+						<th>Nr</th>
+						<th>Matrikel nr.</th>
+						<th>Antal lejemål</th>
+						<th></th>
+						@foreach ($company->realestates as $estate)
+						<tr>
+							<td>{{$estate->id}}</td>
+							<td>{{$estate->street_name}}</td>
+							<td>{{$estate->street_number}}</td>
+							<td>{{$estate->cadastral_number}}</td>
+							<td>{{$estate->leases}}</td>
+							<td><span class="glyphicon glyphicon-chevron-right"></span></td>
+						</tr>
+						@endforeach
+					</table>
+					@else
+					Der er ingen bygninger i dette firma...
+					@endif
 
+				</div>
 			</div>
+			@endforeach
 		</div>
-		@endforeach
 	</div>
-</div>
 </div>
 
 @stop
