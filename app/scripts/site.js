52322c17558b20ab7ca64c3e3c1d1b6b31b3d5df
Mads Dam Eckardt Jensen <mdej35@hotmail.com>
2014-10-03 12:59:46 +0200

    Mads

--- app/scripts/site.js
+++ app/scripts/site.js
@@ -4,8 +4,33 @@ function searchStr(str, array) {
 	for(i = 0; i < array.length; i++) {
 		for(j = 0; j < array[i].length; j++) {
 			if(array[i][j].toLowerCase().indexOf(str.toLowerCase()) != -1) {
-				result.push(users[i]);
+				result.push(array[i]);
+				break;
 			}
+		}
+	}
+	return result;
+}
+
+// Highligther function
+function highlight(key, text, before, after) {
+
+	if(key == '' || text == '') {
+		return text;
+	}
+	var result = '';
+	var tmpStr = text;
+	var keySize = key.length;
+	while(tmpStr.length != 0) {
+		var firstPos = tmpStr.toLowerCase().indexOf(key.toLowerCase());
+		if(firstPos == -1) {
+			result += tmpStr;
+			break;
+		}
+		else {
+			result += tmpStr.substr(0, firstPos);
+			result += before + tmpStr.substr(firstPos, keySize) + after;
+			tmpStr = tmpStr.substr(firstPos + keySize);
 		}
 	}
 	return result;
