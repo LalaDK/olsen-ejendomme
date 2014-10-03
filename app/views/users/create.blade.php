a4c186ce7c0e43b8d7c8082cf5fbd0fa9cac36d3
Mads Dam Eckardt Jensen <mdej35@hotmail.com>
2014-10-03 16:26:08 +0200

    mads

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -1,19 +1,12 @@
 @extends('layouts.lightbox')
-
-
-
-
 @section('content')
-<div class="container-fluid box create-user-box">
-<div class="title-text">Opret bruger</div>
-<a href='#' onclick="closeLitebox();">Mads</a>
-
+<div class="create-user-box box"> 
+	<div class="title-text">Opret bruger</div>
 	{{Form::open(['route' => 'users.store'])}}
 	{{Form::text('name', '', array('class' => 'form-control', 'placeholder' => 'Navn'))}}
 	{{Form::text('username', '', array('class' => 'form-control', 'placeholder' => 'E-mail'))}}
 	{{Form::password('password', array('class' => 'form-control', 'placeholder' => 'Kodeord'))}}
 	{{Form::submit('Opret bruger', array('class' => 'btn btn-success button'))}}
 	{{ Form::close() }}
-	
 </div>
 @stop
--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -7,22 +7,38 @@ var users = new Array();
 
 
 // Function to return divs
-function userFormat(arr) {
-	var result = '';
-	before = '<b>';
-	after = '</b>';
+function onSearch(searchString) {
+	var arr = searchStr(searchString, users);		// Array containing users which matched the search
+	var result = ''; 								// Result containing div elements to write to doc
+
 	for(i = 0; i < arr.length; i++) {
 		var key = document.getElementById('search').value;
 		var text = arr[i][1] + ' - ' + arr[i][2];
+
 		result += '<div class="search-result">' 
-		+ highlight(key, text, before, after)
-		+ '<div class="search-result-delete"><a href="#">Slet bruger x</a></div></div>';
+		+ highlight(key, text, '<b>', '</b>')
+		+ '<div class="search-result-delete">' 
+		+ '<a href="#" onClick="deleteUser('+ arr[i][0] +');">Slet bruger x</a>'
+		+ '</div>'
+		+ '</div>';
 	}
-	document.getElementById('result').innerHTML = result;
-}
+	document.getElementById('result').innerHTML = result;	// Post result to page
+};
+
+function deleteUser(id) {
+	var confirmed = confirm("Er du sikker på at du vil slette denne bruger?");
+	if(confirmed) {
+		$.ajax({
+			url: document.URL + '/' + id,
+			type: 'DELETE',
+			success: function(result) {
+				alert('Brugeren blev slettet!');
+			}
+		});
+	}
+};
 
 </script>
-
 
 <!-- Snippet for passing all users from php/blade to javascript array -->
 @foreach ($users as $user)
@@ -32,10 +48,17 @@ function userFormat(arr) {
 </script>
 @endforeach
 
-<div class="box"> 
+
+
+<div class="box" id="delete-user"> 
 	<div class="title-text">Slet lejer</div>
 	<b>Søg efter brugere som skal slettes fra systemet</b><br><br>
-	<input type="text" id="search" placeholder="Indtast navn eller ID" class="form-control" onkeyup="userFormat(searchStr(this.value, users));">
+	
+	<input type="text" id="search" 
+	placeholder="Indtast navn eller ID" 
+	class="form-control" 
+	onkeyup="onSearch(this.value);">
+
 	<div id="result"></div><br>
 	<button type="button" class="btn btn-success button">Luk</button>
 </div>
